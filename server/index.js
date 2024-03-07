9719dfcfa1da2614fcf0a73df3aa7fe6da157b2f
Prateek Maurya <83070760+PrateekMaurya2@users.noreply.github.com>
2024-03-07 21:08:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
