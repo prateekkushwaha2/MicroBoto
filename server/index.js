db35a70f7494b16936aaa5e8b2cafadcd0f7e3a5
Prateek Maurya <83070760+PrateekMaurya2@users.noreply.github.com>
2024-03-07 21:47:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 3306;
 dotenv.config();
 app.use(cors());
 
-
+console.log('Database Connection Details:', {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  // ... other details
+});
 // Database setup
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -24,6 +28,17 @@ const pool = mysql.createPool({
   connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
   queueLimit: 0
 });
+pool.getConnection()
+  .then(() => {
+    console.log('Connected to the database!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  })
+  .finally(() => {
+    // Release the connection if it was acquired
+    pool.end();
+  });
 app.get("/", (req, res) => res.send("Express on Vercel"));
 // Middleware for parsing requests
 app.use(bodyParser.urlencoded({ extended: true }));
